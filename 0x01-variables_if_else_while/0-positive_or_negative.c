b2cf949d040081cf8a4f7b2896a302b7bf9137c3
snipher marube <sniphermarube@gmail.com>
2023-02-18 03:17:23 -0800

    random

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,13 +6,11 @@
 /* betty style doc for function main goes there */
 /**
  * main - Entry pont
- *
  * This program will assign a random 
  * number to the variable n each time it is executed.
  * Complete the source code in order
  * to print whether the number stored in
  * the variable n is positive or negative.
- *
  * Return: Always 0 (Success)
  */
 int main(void)
