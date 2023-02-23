c1db69d38b8345d381a5762e51919888f06515ec
snipher marube <sniphermarube@gmail.com>
2023-02-23 08:04:09 -0800

    squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - function to print squares
+ * @size : integer value
+ * Return: Always 0.
+ */
+
+void print_square(int size)
+{
+	int row, col;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (row = 0; row < size; row++)
+	{
+		for (col = 0; col < size; col++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
