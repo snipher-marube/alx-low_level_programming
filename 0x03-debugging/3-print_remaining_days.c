bd330fc7d08dfe0042f9dea5472ea47fa4bc1b7f
snipher marube <sniphermarube@gmail.com>
2023-02-23 02:24:49 -0800

    leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,70 +12,26 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-    int total_days = 0;
-    int remaining_days = 0;
-    int is_leap_year = 0;
+	if ((year % 4 == 0) || (year % 400 == 0 && year % 100 == 0))
+	{
+		if (month > 2 && day >= 60)
+		{
+			day++;
+		}
 
-    if (year % 4 == 0)
-    {
-        if (year % 100 == 0)
-        {
-            if (year % 400 == 0)
-            {
-                is_leap_year = 1;
-            }
-        }
-        else
-        {
-            is_leap_year = 1;
-        }
-    }
-
-    switch (month)
-    {
-        case 1:
-            total_days = 31;
-            break;
-        case 2:
-            total_days = is_leap_year ? 29 : 28;
-            break;
-        case 3:
-            total_days = 31 + 28 + is_leap_year;
-            break;
-        case 4:
-            total_days = 31 + 28 + is_leap_year + 31;
-            break;
-        case 5:
-            total_days = 31 + 28 + is_leap_year + 31 + 30;
-            break;
-        case 6:
-            total_days = 31 + 28 + is_leap_year + 31 + 30 + 31;
-            break;
-        case 7:
-            total_days = 31 + 28 + is_leap_year + 31 + 30 + 31 + 30;
-            break;
-        case 8:
-            total_days = 31 + 28 + is_leap_year + 31 + 30 + 31 + 30 + 31;
-            break;
-        case 9:
-            total_days = 31 + 28 + is_leap_year + 31 + 30 + 31 + 30 + 31 + 31;
-            break;
-        case 10:
-            total_days = 31 + 28 + is_leap_year + 31 + 30 + 31 + 30 + 31 + 31 + 30;
-            break;
-        case 11:
-            total_days = 31 + 28 + is_leap_year + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31;
-            break;
-        case 12:
-            total_days = 31 + 28 + is_leap_year + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30;
-            break;
-        default:
-            break;
-    }
-
-    remaining_days = total_days - day;
-
-    printf("Day of the year: %d\n", day + is_leap_year);
-    printf("Remaining days: %d\n", remaining_days + is_leap_year);
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
+	}
+	else
+	{
+		if (month == 2 && day == 60)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
+		else
+		{
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
+		}
+	}
 }
-
