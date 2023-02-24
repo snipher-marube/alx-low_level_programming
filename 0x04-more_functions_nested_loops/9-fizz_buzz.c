5d2499b825fa138a5410f1bfdf9f3d35c7c50ae8
snipher marube <sniphermarube@gmail.com>
2023-02-23 20:56:42 -0800

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
+	int i;
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
@@ -23,7 +24,11 @@ int main(void)
 		}
 		else
 		{
-			print("%d", i);
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
 		}
 	}
 	printf("\n");
