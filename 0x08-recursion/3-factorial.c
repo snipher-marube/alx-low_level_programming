5ea790045a6062ae317558499c9228bd1253d271
snipher marube <sniphermarube@gmail.com>
2023-03-08 07:51:13 -0800

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - prints factorial of a number
+ * @n: number fuctorial
+ * Return: factorial total
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
