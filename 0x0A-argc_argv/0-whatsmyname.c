7347cb636711e0b23d329d0318f261b5aeca1d17
snipher marube <sniphermarube@gmail.com>
2023-03-10 00:01:26 -0800

    displaying the name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print thr name of the file
+ * @argc: first argument
+ * @argv: second agument
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
