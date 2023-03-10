df37b6acda96a3d9595fad031d75116d5bca764c
snipher marube <sniphermarube@gmail.com>
2023-03-10 00:06:54 -0800

    prints number of arguments passed into it

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/** File: Main.h
+ *  Description - This file contains all the function prototypes used in this project.
+ */
+
+int _putchar (char c);
+int main(int argc, char *argv[]);
+
+#endif
